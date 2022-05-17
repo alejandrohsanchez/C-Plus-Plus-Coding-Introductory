644d534db800c6a6f4eb21cd3598b3a8ee47f7b7
alejandrohsanchez <52898001+alejandrohsanchez@users.noreply.github.com>
2022-05-17 15:34:40 -0700

    Updated sample code

---
+++ Code1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+bool solution(string S) {
+    cout << S.length() << endl;
+
+    return false;
+}
+
+int main() {
+    String name = "Alejandro";
+    bool c = solution(name);
+}
